9410a32ac9dee78e4322eb79eeb47f6ce2d78697
Nelaev Vitaly <worknelq@gmail.com>
2026-05-19 10:38:16 +0700

    123

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,35 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'screens/auth_screen.dart';
-
 // main.dart
 import 'services/background_notification_service.dart';
-
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // Обработка, когда приложение закрыто
+  print("Фоновое сообщение: ${message.data}");
+}
+// Это должно работать даже с warnings
+void _handleMessage(RemoteMessage message) {
+  final requestId = message.data['request_id'];
+  print("✅  ${message.toString()}");
+}
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await BackgroundNotificationService.initialize(); // <-- Запуск сервиса
 
 
   runApp(MyApp());
-  
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+// Добавьте логи для проверки
+FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+  print("✅ Уведомление получено: ${message.data.toString()}");
+  // Должно выводиться даже с warnings
+});
+
+FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+  print("✅ Нажато на уведомление");
+  _handleMessage(message); // Ваш переход на заявку
+}
+);
 }
 
 class MyApp extends StatelessWidget {
